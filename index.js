bf4494b7cab0794b7c9ef6301ca59780b26aecd0
Valiantsin Labanau <valiantsin.labanau@itechart-group.com>
2022-07-13 23:11:30 +0300

    Add file

--- index.js
+++ index.js
@@ -10,15 +10,19 @@ const PORT = process.env.PORT || 9999;
 const server = http.createServer(app);
 
 async function sendDataToFile(data) {
-  await fs.open(path(__dirname, "data.txt"), "w", (err) => {
+  await fs.open(path.join(__dirname, "data.txt"), "w", (err) => {
     if (err) throw err;
     console.log("File created");
   });
 
-  await fs.appendFile(path(__dirname, "data.txt"), toString(data), (err) => {
-    if (err) throw err;
-    console.log("Data has been added!");
-  });
+  await fs.appendFile(
+    path.join(__dirname, "data.txt"),
+    toString(data),
+    (err) => {
+      if (err) throw err;
+      console.log("Data has been added!");
+    }
+  );
 }
 
 app.get("/", (req, res) => {
@@ -28,7 +32,7 @@ app.get("/", (req, res) => {
 app.get("/send", (req, res) => {
   console.log(req);
   sendDataToFile("data");
-  fs.readFile(path(__dirname, "data.txt"), (err, data) => {
+  fs.readFile(path.join(__dirname, "data.txt"), (err, data) => {
     if (err) throw err;
     res.send(data);
   });
