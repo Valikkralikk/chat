695f1b460d6da5fefc72b2b5fe125a2a59420936
Valiantsin Labanau <valiantsin.labanau@itechart-group.com>
2023-07-13 11:53:40 +0300

    update

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
+const express = require("express");
+const { botStart } = require("./services/telegramBot");
+const app = express();
 const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+    botStart();
+});
--- services/telegramBot/index.js
+++ services/telegramBot/index.js
@@ -1,27 +1,14 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.botStart = void 0;
-const node_telegram_bot_api_1 = __importDefault(require("node-telegram-bot-api"));
+const TelegramBot = require("node-telegram-bot-api");
 const token = '6304468525:AAGGEVpySqbrLjQVFDmQ8ABlXBnGyOmoUbs';
-const bot = new node_telegram_bot_api_1.default(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
 bot.on('message', (msg) => {
-    console.log(msg);
     const chatId = msg.chat.id;
     if (msg.text == 'dog' || msg.text == '🦮') {
         bot.sendMessage(chatId, "You sent 'dog'");
     }
 });
-const botStart = () => __awaiter(void 0, void 0, void 0, function* () { return yield bot; });
-exports.botStart = botStart;
+const botStart = async () => {
+    console.log(`Bot started`);
+    return await bot;
+};
+module.exports = { botStart };
