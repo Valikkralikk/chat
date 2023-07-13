036f7b1d96f433423ebf64466563436448fdff9a
Valiantsin Labanau <valiantsin.labanau@itechart-group.com>
2023-07-13 11:14:05 +0300

    comment puppiter

--- app.js
+++ app.js
@@ -15,30 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
 const express_1 = __importDefault(require("express"));
 const users_1 = require("./routes/users/");
-const puppeteer_1 = __importDefault(require("puppeteer"));
 const telegramBot_1 = require("./services/telegramBot");
-const getAmwayPage = () => __awaiter(void 0, void 0, void 0, function* () {
-    const url = new URL('https://www.kz.amway.com/search/');
-    url.pathname = 'search';
-    url.searchParams.append('text', '0014');
-    const browser = yield puppeteer_1.default.launch({ headless: false });
-    const page = yield browser.newPage();
-    console.log("We are scraping from " + url + ":");
-    yield page.goto(url.toString());
-    let hrefs = yield page.evaluate(() => {
-        let Element = Array.from(document.body.querySelectorAll('div[data-product-id]'), (el) => {
-            let data = el.getAttribute('data-tealium');
-            if (data) {
-                data = JSON.parse(data);
-            }
-            return data;
-        });
-        return Element;
-    });
-    yield browser.close();
-    const obj = hrefs[0];
-    yield console.log(obj);
-});
 class App {
     constructor(logger) {
         this.app = (0, express_1.default)();
@@ -54,7 +31,6 @@ class App {
             this.server = this.app.listen(this.PORT, () => {
                 (0, telegramBot_1.botStart)();
                 this.logger.log(`Server started on ${this.PORT}`);
-                getAmwayPage();
             });
         });
     }
