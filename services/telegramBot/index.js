e6f8c4a582a8c536d7ada7ef92a9d869dd5ab7b9
Valiantsin Labanau <valiantsin.labanau@itechart-group.com>
2023-07-13 11:57:19 +0300

    update

--- services/telegramBot/index.js
+++ services/telegramBot/index.js
@@ -3,6 +3,7 @@ const token = '6304468525:AAGGEVpySqbrLjQVFDmQ8ABlXBnGyOmoUbs';
 const bot = new TelegramBot(token, { polling: true });
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
+    console.log(msg.text);
     if (msg.text == 'dog' || msg.text == '🦮') {
         bot.sendMessage(chatId, "You sent 'dog'");
     }
