f95175ee1fcceba7aa5dfea6697bb4357fcf8a67
Valiantsin Labanau <valiantsin.labanau@itechart-group.com>
2023-07-13 12:07:41 +0300

    update

--- main.js
+++ main.js
@@ -5,5 +5,8 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
+    setInterval(()=>{
+        console.log('dont sleep')
+    },1000);
     botStart();
 });
