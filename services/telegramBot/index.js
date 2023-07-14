02dffa1682b223e2706fb61fd1846588fdca4749
Valiantsin Labanau <valiantsin.labanau@itechart-group.com>
2023-07-14 11:42:38 +0300

    update

--- services/telegramBot/index.js
+++ services/telegramBot/index.js
@@ -25,6 +25,7 @@ class Bot {
 
         this.bot.on('message', async (msg) => {
             const chatId = msg.chat.id;
+            console.log(msg.text);
             if (Number(msg.text)) {
                 if (this.checkProductInCash(msg.text)) {
                     const productInCash = this.checkProductInCash(msg.text)
