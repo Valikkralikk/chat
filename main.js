37db50b4d9d314d6a2790ad23e7017d73d62bc25
Valiantsin Labanau <valiantsin.labanau@itechart-group.com>
2023-07-14 12:04:42 +0300

    add timer

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const { bot } = require("./services/telegramBot");
 const app = express();
 const port = 3000;
 
+function timer(time) {
+    setTimeout(() => {
+        console.log(time);
+        timer(Math.round(Math.random() * 120000));
+    }, time);
+}
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
     bot.initBot();
-    setInterval(() => {
-        console.log('Dont sleep', + new Date());
-    }, 60000)
+    timer(60000);
 });
