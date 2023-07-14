320a48a012334d13477d3e011857a72769bc6eae
Valiantsin Labanau <valiantsin.labanau@itechart-group.com>
2023-07-14 14:35:37 +0300

    delete timer

--- main.js
+++ main.js
@@ -4,13 +4,6 @@ const { default: axios } = require("axios");
 const app = express();
 const port = 3000;
 
-function timer(time) {
-    setTimeout(() => {
-        console.log(time);
-        timer(Math.round(Math.random() * 120000));
-    }, time);
-}
-
 app.get('/', async (req, res) => {
     const response = await axios('https://dont-sleep.onrender.com');
     console.log(response.data);
@@ -19,5 +12,4 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
     bot.initBot();
-    timer(60000);
 });
